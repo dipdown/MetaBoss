c4a74fdd380270e93923d804d05da1b88b8f3c23
Ilham Riky Rismawan <166375320+dipdown@users.noreply.github.com>
2024-05-15 02:39:38 +0700

    Delete src/account.js

--- src/account.js
+++
@@ -1,8 +0,0 @@
-const account = [
-  [123123123, "Account Name"], //account 1
-  [123123123, "Account Name"], //account 2
-  [123123123, "Account Name"], //account 3
-  [123123123, "Account Name"], //account 4
-];
-
-export { account };
